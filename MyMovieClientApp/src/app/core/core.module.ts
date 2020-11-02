c99f8361873fd6b15b390ad3c1c9f78fe8138c0a
kgonzalez712 <kevin.gonzalezs0795@gmail.com>
2020-11-01 20:12:46 -0600

    Added http service

--- MyMovieClientApp/src/app/core/core.module.ts
+++ MyMovieClientApp/src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [NavMenuComponent, AppLayoutComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   exports: [
     AppLayoutComponent
