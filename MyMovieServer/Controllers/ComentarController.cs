6841ae53fdff84bea8eb54c4046e02d9cc152149
Luis Corrales Solano <luis.corrales1197@gmail.com>
2020-11-06 19:16:27 -0600

    Added Controller and logic to server

---
+++ MyMovieServer/Controllers/ComentarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyMovieServer.Models;
+
+namespace MyMovieServer.Controllers
+{
+    [Route("comentar")]
+    [ApiController]
+    public class ComentarController : ControllerBase
+    {
+        private ILogger<ComentarController> _logger;
+        private MyMovieDBContext _context;
+
+    }
+}
---
+++ MyMovieServer/Logic/LogicaAgregarComentario.cs
@@ -0,0 +1,35 @@
+﻿using MyMovieServer.Models;
+using MyMovieServer.Presentation_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMovieServer.Logic
+{
+    public class LogicaAgregarComentario
+    {
+        public int Comentar(ComentariosPM c, int idPeli, MyMovieDBContext context)
+        {
+            try
+            {
+                Calificacion comentario = new Calificacion
+                {
+                    Comentario = c.Comentario,
+                    Calificacion1 = c.Calificacion,
+                    IdPelicula = idPeli
+                };
+                context.Add(comentario);
+                context.SaveChanges();
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+        }
+
+
+    }
+}
