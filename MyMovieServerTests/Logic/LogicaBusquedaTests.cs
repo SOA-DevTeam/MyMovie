9654ae689b20be71e621a20369dd7096c9c60b46
kgonzalez712 <kevin.gonzalezs0795@gmail.com>
2020-11-08 21:18:36 -0600

    Fixed Test error

--- MyMovieServerTests/Logic/LogicaBusquedaTests.cs
+++ MyMovieServerTests/Logic/LogicaBusquedaTests.cs
@@ -23,7 +23,7 @@ namespace MyMovieServer.Logic.Tests
 
             //Al existir solo un elemento con ese nombre en la base de datos, se comprueba que sea una lista de un elemento
             //Tambien se comprueba que los datos son los mismos del elemento en la base de datos
-            var isEqual = request.Count == 1 && response.idPelicula == 1493 && response.NombrePelicula.Equals("The TEST Movie")
+            var isEqual = request.Count == 2 && response.idPelicula == 1493 && response.NombrePelicula.Equals("The TEST Movie")
                 && response.Director.Equals("Mark Tester") && response.AnoPelicula.Equals("2020");
 
             //Si el elemento es igual, se pasa la prueba
