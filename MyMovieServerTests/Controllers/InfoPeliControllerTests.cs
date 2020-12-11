6c649e3e8e94fd0d428a0674e3612441771e0c7c
Luis Corrales Solano <luis.corrales1197@gmail.com>
2020-12-11 15:26:42 -0600

    ModifyTest

---
+++ MyMovieServerTests/Controllers/InfoPeliControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMovieServer.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using MyMovieServer.Mapping;
+using MyMovieServer.Repositories;
+using MyMovieServer.UoW;
+using MyMovieServer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMovieServer.Controllers.Tests
+{
+    [TestClass()]
+    public class InfoPeliControllerTests
+    {
+        Mock<IKeyValueMap> mapper = new Mock<IKeyValueMap>();
+        Mock<IInfoPeliRepo> repository = new Mock<IInfoPeliRepo>();
+        Mock<IUnitOfWork> unitOfWork = new Mock<IUnitOfWork>();
+
+        [TestMethod()]
+        public void GetGPMTest()
+        {
+            IInfoPeliRepo repo = new MockInfoPeliRepo();
+
+            var prueba = new InfoPeliController(repo, mapper.Object);
+            var generos = prueba.GetGPM();
+            Assert.IsInstanceOfType(generos, typeof(ObjectResult));
+            var response = generos as ObjectResult;
+            Assert.AreEqual(200, response.StatusCode);
+
+        }
+
+        [TestMethod()]
+        public void GetIPMTest()
+        {
+            IInfoPeliRepo repo = new MockInfoPeliRepo();
+
+            var prueba = new InfoPeliController(repo, mapper.Object);
+            var idiomas = prueba.GetIPM();
+            Assert.IsInstanceOfType(idiomas, typeof(ObjectResult));
+            var response = idiomas as ObjectResult;
+            Assert.AreEqual(200, response.StatusCode);
+        }
+
+        [TestMethod()]
+        public void GetEPMTest()
+        {
+            IInfoPeliRepo repo = new MockInfoPeliRepo();
+
+            var prueba = new InfoPeliController(repo, mapper.Object);
+            var estilos = prueba.GetEPM();
+            Assert.IsInstanceOfType(estilos, typeof(ObjectResult));
+            var response = estilos as ObjectResult;
+            Assert.AreEqual(200, response.StatusCode);
+        }
+    }
+}
---
+++ MyMovieServerTests/Controllers/PeliculasControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MyMovieServer.Controllers;
+using MyMovieServer.Mapping;
+using MyMovieServer.Models;
+using MyMovieServer.Presentation_Model;
+using MyMovieServer.Repositories;
+using MyMovieServer.UoW;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyMovieServer.Controllers.Tests
+{
+    [TestClass()]
+    public class PeliculasControllerTests
+    {
+        Mock<IPeliculaMap> mapper = new Mock<IPeliculaMap>();
+        Mock<IPeliculasRepo> repository = new Mock<IPeliculasRepo>();
+        Mock<IUnitOfWork> unitOfWork = new Mock<IUnitOfWork>();
+
+        [TestMethod()]
+        public void AddPeliculaTest()
+        {
+            IPeliculasRepo repo = new MockPeliculasRepo();
+
+            var prueba = new PeliculasController(repo, unitOfWork.Object,mapper.Object);
+            var pelicula = new NuevaPeliculaPM();
+            pelicula.nombre = "Pelicula nueva";
+            pelicula.director = "Director";
+            pelicula.anno = 2000;
+            pelicula.mdb = 8;
+            pelicula.meta = 8;
+            pelicula.fav = false;
+            pelicula.idioma = 1;
+            pelicula.genero = 1;
+            pelicula.estilo = 1;
+            pelicula.imagen = "imagen";
+            pelicula.pop = 10;
+
+            
+            try { 
+                prueba.AddPelicula(pelicula);
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+                Assert.Fail();
+            }
+            
+        }
+
+        [TestMethod()]
+        public void UpdatePeliculaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetComentariosTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
